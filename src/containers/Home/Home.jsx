4e3e18f114ce3249820ed8b4cc9e8aa7e3a7291e
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-03-07 21:24:22 +0100

    render state to DOM

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,75 +1,36 @@
-import logo from '../../assets/ai.png';
+import { useSelector, useDispatch } from 'react-redux';
+import { reserveRocket } from './rocketSlice';
 import './Home.css';
 
-const Home = () => (
-  <div className="home">
+const Home = () => {
+  const { rockets } = useSelector((store) => store.rocket);
+  const dispatch = useDispatch();
+  const displayRockets = rockets.length ? (
     <ul>
-      <li>
-        <div>
-          <img src={logo} height="60px" alt="logo" />
-        </div>
-        <div>
-          <h2>Falcon 1</h2>
-          <p>
-            The Rockets section displays a list of all available SpaceX rockets.
-            Users can book each rocket by clicking the reservation button
-            or cancel the previously made booking.
-            The same layout is used to form the Dragons section
-            [added only if your team has 3 members].
-          </p>
-          <button type="button">Reserve Rocket</button>
-        </div>
-      </li>
-      <li>
-        <div>
-          <img src={logo} height="60px" alt="logo" />
-        </div>
-        <div>
-          <h2>Falcon 1</h2>
-          <p>
-            The Rockets section displays a list of all available SpaceX rockets.
-            Users can book each rocket by clicking the reservation button
-            or cancel the previously made booking.
-            The same layout is used to form the Dragons section
-            [added only if your team has 3 members].
-          </p>
-          <button type="button">Reserve Rocket</button>
-        </div>
-      </li>
-      <li>
-        <div>
-          <img src={logo} height="60px" alt="logo" />
-        </div>
-        <div>
-          <h2>Falcon 1</h2>
-          <p>
-            The Rockets section displays a list of all available SpaceX rockets.
-            Users can book each rocket by clicking the reservation button
-            or cancel the previously made booking.
-            The same layout is used to form the Dragons section
-            [added only if your team has 3 members].
-          </p>
-          <button type="button">Reserve Rocket</button>
-        </div>
-      </li>
-      <li>
-        <div>
-          <img src={logo} height="60px" alt="logo" />
-        </div>
-        <div>
-          <h2>Falcon 1</h2>
-          <p>
-            The Rockets section displays a list of all available SpaceX rockets.
-            Users can book each rocket by clicking the reservation button
-            or cancel the previously made booking.
-            The same layout is used to form the Dragons section
-            [added only if your team has 3 members].
-          </p>
-          <button type="button">Reserve Rocket</button>
-        </div>
-      </li>
+      {
+      rockets.map((rocket) => (
+        <li key={rocket.id}>
+          <div>
+            <img src={rocket.image} height="60px" alt="logo" />
+          </div>
+          <div>
+            <h2>{rocket.title}</h2>
+            <p>
+              {rocket.description}
+            </p>
+            <button type="button" onClick={() => dispatch(reserveRocket(rocket.id))}>Reserve Rocket</button>
+          </div>
+        </li>
+      ))
+    }
     </ul>
-  </div>
-);
+  ) : (<p>there is no rocket on the list</p>);
+
+  return (
+    <div>
+      {displayRockets}
+    </div>
+  );
+};
 
 export default Home;
