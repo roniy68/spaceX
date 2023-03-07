4885ca51f66ebe56843d45ff5e38870b619d07ae
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-03-07 18:31:16 +0100

    hard code data to be used

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,7 +3,64 @@ import './Home.css';
 
 const Home = () => (
   <div className="home">
-    Home Component
+    <ul>
+      <li>
+        <div />
+        <div>
+          <h2>Falcon 1</h2>
+          <p>
+            The Rockets section displays a list of all available SpaceX rockets.
+            Users can book each rocket by clicking the reservation button
+            or cancel the previously made booking.
+            The same layout is used to form the Dragons section
+            [added only if your team has 3 members].
+          </p>
+          <button type="button">Reserve Rocket</button>
+        </div>
+      </li>
+      <li>
+        <div />
+        <div>
+          <h2>Falcon 1</h2>
+          <p>
+            The Rockets section displays a list of all available SpaceX rockets.
+            Users can book each rocket by clicking the reservation button
+            or cancel the previously made booking.
+            The same layout is used to form the Dragons section
+            [added only if your team has 3 members].
+          </p>
+          <button type="button">Reserve Rocket</button>
+        </div>
+      </li>
+      <li>
+        <div />
+        <div>
+          <h2>Falcon 1</h2>
+          <p>
+            The Rockets section displays a list of all available SpaceX rockets.
+            Users can book each rocket by clicking the reservation button
+            or cancel the previously made booking.
+            The same layout is used to form the Dragons section
+            [added only if your team has 3 members].
+          </p>
+          <button type="button">Reserve Rocket</button>
+        </div>
+      </li>
+      <li>
+        <div />
+        <div>
+          <h2>Falcon 1</h2>
+          <p>
+            The Rockets section displays a list of all available SpaceX rockets.
+            Users can book each rocket by clicking the reservation button
+            or cancel the previously made booking.
+            The same layout is used to form the Dragons section
+            [added only if your team has 3 members].
+          </p>
+          <button type="button">Reserve Rocket</button>
+        </div>
+      </li>
+    </ul>
   </div>
 );
 
--- src/containers/Missions/Missions.jsx
+++ src/containers/Missions/Missions.jsx
@@ -1,7 +1,36 @@
 const Missions = () => (
-  <div>
-    Missions
-  </div>
+  <table>
+    <tr>
+      <th>missions</th>
+      <th>description</th>
+      <th>status</th>
+      <th>s/n</th>
+    </tr>
+    <tr>
+      <td>thiacom</td>
+      <td>thiacom</td>
+      <td>thiacom</td>
+      <td>thiacom</td>
+    </tr>
+    <tr>
+      <td>thiacom</td>
+      <td>thiacom</td>
+      <td>thiacom</td>
+      <td>thiacom</td>
+    </tr>
+    <tr>
+      <td>thiacom</td>
+      <td>thiacom</td>
+      <td>thiacom</td>
+      <td>thiacom</td>
+    </tr>
+    <tr>
+      <td>thiacom</td>
+      <td>thiacom</td>
+      <td>thiacom</td>
+      <td>thiacom</td>
+    </tr>
+  </table>
 );
 
 export default Missions;
