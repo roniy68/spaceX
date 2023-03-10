742203057ff9d9d9b2e675fd46e3fd1ab18d3f5a
Nwadinigwe Victor chiemerie <victorchiemerie02@gmail.com>
2023-03-10 12:44:19 +0100

    Revert "🔥  Feature/test : Add All Feature Together and Test"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
-import Icon from 'assets/planet.png';
+import Icon from '../../assets/planet.png';
 
 export default function Navbar() {
   return (
